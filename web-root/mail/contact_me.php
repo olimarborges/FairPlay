91962c9e01b5fcae63aa72aee4dd35fb98d543b5
Olimar Teixeira B <olimarbjunior@gmail.com>
2018-02-14 16:23:28 -0200

    Versão inicial do site

---
+++ web-root/mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+	
+// Create the email and send the message
+$to = 'olimar.borges@acad.pucrs.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulário de contato Fair PlLay:  $name";
+$email_body = "Você recebeu uma nova mensagem do formulário de contato do site do Fair Play.\n\n"."Aqui estão os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@acad.pucrs.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
